f3cd891581e6948dad3649848cbf09729b22afbe
ChenLiang <chenliang@chenliangdeimac.local>
2017-01-16 16:34:41 +0800

    Initial Commit

---
+++ WeChatLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChatLogin
+//
+//  Created by ChenLiang on 17/1/16.
+//  Copyright © 2017年 wechat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChatLogin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeChatLogin
+//
+//  Created by ChenLiang on 17/1/16.
+//  Copyright © 2017年 wechat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
