20ef29f5797acf1d6c9656dcc1f3b36e9ebf8cad
18377613852 <chenliang@ChenLiangdeiMac.local>
2017-01-16 18:08:41 +0800

    修改
    修改冲突

--- WeChatLogin/ViewController.m
+++ WeChatLogin/ViewController.m
@@ -19,13 +19,13 @@
     // Do any additional setup after loading the view, typically from a nib.
     UIButton *btn = [UIButton buttonWithType:0];
     [btn setFrame:CGRectMake(200, 30, 80, 80)];
-<<<<<<< HEAD
+
     [btn setTitle:@"normal111" forState:UIControlStateNormal];
     [btn setTitle:@"seleted" forState:UIControlStateSelected];
-=======
+
     [btn setTitle:@"normal" forState:UIControlStateNormal];
     [btn setTitle:@"seleted11" forState:UIControlStateSelected];
->>>>>>> origin/master
+
     btn.backgroundColor = [UIColor redColor];
     [btn addTarget:self action:@selector(gogogo:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
@@ -35,12 +35,12 @@
 - (void)gogogo:(UIButton *)sender{
     sender.selected = !sender.selected;
 //    sender.transform = CATransform3DMakeScale(1, 2, 2);
-<<<<<<< HEAD
+
     //111normal
-=======
+
     //5456
     
->>>>>>> origin/master
+
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
