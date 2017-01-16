13c1dc7b4d397860c2f551162af507ce89d9380f
18377613852 <liushi@MacBook-Pro.local>
2017-01-16 17:48:34 +0800

    sender
    sender.seleted

--- WeChatLogin/ViewController.m
+++ WeChatLogin/ViewController.m
@@ -18,14 +18,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     UIButton *btn = [UIButton buttonWithType:0];
-    [btn setFrame:CGRectMake(200, 30, 30, 30)];
-    [btn setTitle:@"sds" forState:UIControlStateNormal];
+    [btn setFrame:CGRectMake(200, 30, 80, 80)];
+    [btn setTitle:@"normal" forState:UIControlStateNormal];
+    [btn setTitle:@"seleted" forState:UIControlStateSelected];
     btn.backgroundColor = [UIColor redColor];
+    [btn addTarget:self action:@selector(gogogo:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
     
 }
 
-
+- (void)gogogo:(UIButton *)sender{
+    sender.selected = !sender.selected;
+//    sender.transform = CATransform3DMakeScale(1, 2, 2);
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
