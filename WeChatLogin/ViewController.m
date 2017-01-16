dccfb84e5b4bed4a91b34cf08aaa5d89f03c1a0b
18377613852 <chenliang@ChenLiangdeiMac.local>
2017-01-16 17:06:47 +0800

    btn
    add ban

--- WeChatLogin/ViewController.m
+++ WeChatLogin/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn = [UIButton buttonWithType:0];
+    [btn setFrame:CGRectMake(200, 30, 30, 30)];
+    [btn setTitle:@"sds" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
+    
 }
 
 
