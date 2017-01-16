c50b14dcc2a504dd357191692188216ff1b47eec
18377613852 <chenliang@ChenLiangdeiMac.local>
2017-01-16 17:59:58 +0800

    11
    5456

--- WeChatLogin/ViewController.m
+++ WeChatLogin/ViewController.m
@@ -20,7 +20,7 @@
     UIButton *btn = [UIButton buttonWithType:0];
     [btn setFrame:CGRectMake(200, 30, 80, 80)];
     [btn setTitle:@"normal" forState:UIControlStateNormal];
-    [btn setTitle:@"seleted" forState:UIControlStateSelected];
+    [btn setTitle:@"seleted11" forState:UIControlStateSelected];
     btn.backgroundColor = [UIColor redColor];
     [btn addTarget:self action:@selector(gogogo:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
@@ -30,6 +30,7 @@
 - (void)gogogo:(UIButton *)sender{
     sender.selected = !sender.selected;
 //    sender.transform = CATransform3DMakeScale(1, 2, 2);
+    //5456
     
 }
 - (void)didReceiveMemoryWarning {
